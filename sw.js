62525f56dcd0eb64c82f8870567de0130906187c
diana4hobby-svg <diana4hobby@gmail.com>
2026-09-23 16:20:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const CACHE="saxcoach-v11-2-shell";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{if(e.request.method==="GET"&&x.ok){const c=x.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c))}return x}).catch(()=>caches.match("./index.html")))));
+const CACHE="saxcoach-v11-1-shell";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{if(e.request.method==="GET"&&x.ok){const c=x.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));}return x}).catch(()=>caches.match("./index.html")))));
